40c62ef3c724b360c8844ba5d5623c5b0711ff74
Alex <adkosmachev@edu.hse.ru>
2016-10-08 02:16:10 +0300

    Add info

--- vector.cpp
+++ vector.cpp
@@ -1,67 +1,118 @@
 #include <iostream>
+#include <algorithm>
 
 class LVector {
 public: // публичная секция - тут лежат функции, которые могут быть вызваны у объекта
-	LVector() { // конструктор - функция вызывается при создании объекта
-		size = 0; // задать начальные параметры
-		dataSize = 0;
-		delta = 4;
-	}
+  LVector() { // конструктор - функция вызывается при создании объекта
+    size = 0; // задать начальные параметры
+    dataSize = 0;
+    delta = 4;
+    reallocate(false);
+  }
 
-	~LVector() { // деструктор - вызывается при уничтожении объекта (например конец программы)
-		delete[] data; // очистить память
-	}
+  LVector(int len) {
+    size = len;
+    delta = 4;
+    dataSize = int(len / delta) * delta + delta;
+    reallocate(false);
+  }
 
-	void push_back(int elem) { // добавить элемент в конец
-		if(dataSize == size) {
-			reallocate();
-		}
-		data[size] = elem;
-		size += 1;
-	}
+  LVector(int len, int num) {
+    size = len;
+    delta = 4;
+    dataSize = int(len / delta) * delta + delta;
+    reallocate(false);
+    for(int i = 0; i < size; i++) {
+        data[i] = num;
+    }
+  }
 
-	int at(int index) { // взять элемент под номером (аналог [])
-		if(index >= 0 and index < size) {
-			return data[index];
-		} else {
-			return 0;
-		}
-	}
+  ~LVector() { // деструктор - вызывается при уничтожении объекта (например конец программы)
+    delete[] data; // очистить память
+  }
 
-	int getSize() { // узнать размер массива
-		return size;
-	}
+  void push_back(int elem) { // добавить элемент в конец
+    if(dataSize == size) {
+      reallocate(true);
+    }
+    data[size] = elem;
+    size += 1;
+  }
+
+
+  int &operator [] (int index) { // перегружаем []
+    return data[index];
+  }
+
+  int getSize() { // узнать размер массива
+    return size;
+  }
+
+  int * begin() { // итератор на начало
+    return data;
+  }
+
+  int * end() { // итератор на конец
+    return data + size;
+  }
 
 private: // приватная зона - данные +  фукнции, которые нельзя вызывать извне
-	void reallocate() { // выделяем новое местро для хранения
-		dataSize = size+delta;
-		int * newdata = new int[dataSize];
-		for(int i = 0; i < size; i++) {
-			newdata[i] = data[i];
-		}
-		delete[] data;
-		data = newdata;
-	}
+  void reallocate(bool copy) { // выделяем новое местро для хранения
+    dataSize = dataSize+delta;
+    int * newdata = new int[dataSize];
+    if(copy) {
+        for(int i = 0; i < size; i++) {
+            newdata[i] = data[i];
+        }
+        delete[] data;
+    }
+    data = newdata;
+  }
 
-	// данные объекта
-	int * data; // указатель на данные
-	int size; // количество элементов
-	int dataSize; // количество выделенных ячеек
-	int delta; // количество выделяемых новых ячеек
+  // данные объекта
+  int * data; // указатель на данные
+  int size; // количество элементов
+  int dataSize; // количество выделенных ячеек
+  int delta; // количество выделяемых новых ячеек
 };
 
 int main() {
-	LVector h;
-	h.push_back(3);
-	h.push_back(6);
-	h.push_back(9);
-	h.push_back(12);
-	h.push_back(51);
-	h.push_back(21);
+  LVector h;
+  h.push_back(3);
+  h.push_back(6);
+  h.push_back(9);
+  h.push_back(12);
+  h.push_back(51);
+  h.push_back(21);
 
-	for(int i = 0; i < h.getSize(); i++) {
-		std::cout << h.at(i) << std::endl;
-	}
+  for(int i = 0; i < h.getSize(); i++) {
+    std::cout << h[i] << ' ';
+  }
 
-	return 0;
+  std::cout << std::endl;
+
+  LVector hh(7);
+  for(auto it = hh.begin(); it != hh.end(); it++) {
+    std::cout << *it << ' ';
+  }
+
+  std::cout << std::endl;
+
+  LVector hhh(7, 4);
+  for(auto it = hhh.begin(); it != hhh.end(); it++) {
+    std::cout << *it << ' ';
+  }
+
+  std::cout << std::endl;
+
+  std::sort(h.begin(), h.end(), [](int a, int b){return a > b;});
+
+  for(auto it = h.begin(); it != h.end(); it++) {
+    std::cout << *it << ' ';
+  }
+
+  std::cout << std::endl;
+
+  return 0;
 }
+
